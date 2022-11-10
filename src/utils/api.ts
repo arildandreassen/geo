ee22d1daf7bca42ce4f90b6e8b89609782026232
Arild Andreassen <arildsa@hotmail.com>
2022-11-09 21:00:33 -0700

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,11 @@
-// const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000'
-const API_BASE_URL = 'https://bkrz3yt5p0.execute-api.us-west-1.amazonaws.com/dev/'
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000'
 
 
 const getCountry = async () => {
     console.log(API_BASE_URL)
-    await fetch(API_BASE_URL)
-    .then(response => response.json())
-    .then(console.log)
+    // await fetch(API_BASE_URL)
+    // .then(response => response.json())
+    // .then(console.log)
 }
 
 export {
