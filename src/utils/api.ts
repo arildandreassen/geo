92d01952d2d2114d0d318008430e206eeb58eb86
Arild Andreassen <arildsa@hotmail.com>
2022-11-09 19:13:19 -0700

    updated

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000'
 
 const getCountry = async () => {
+    console.log(API_BASE_URL)
     await fetch(API_BASE_URL)
     .then(response => response.json())
     .then(console.log)
