07a2760dda33c2a8b4f26818990c43421d5ac43f
Arild Andreassen <arildsa@hotmail.com>
2022-11-15 21:00:27 -0700

    renamed env variable

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://bkrz3yt5p0.execute-api.us-west-1.amazonaws.com/dev/'
 
 const getCountry = async () => {
     console.log(API_BASE_URL)
