1fc46e0f0693bb59549b619827d6af6509955ad2
Arild Andreassen <arildsa@hotmail.com>
2022-11-16 06:59:34 -0700

    calling api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,9 @@
 import {config} from '../env/config'
 
 const getCountry = async () => {
-    console.log(config.BASE_URL)
-    console.log(process.env.NODE_ENV)
-    // await fetch(API_BASE_URL)
-    // .then(response => response.json())
-    // .then(console.log)
+    await fetch(config.BASE_URL)
+    .then(response => response.json())
+    .then(console.log)
 }
 
 export {
