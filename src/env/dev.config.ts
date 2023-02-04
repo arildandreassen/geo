8fbfb1b696ac2e6d0a2e609efb179ae2e52bbbd9
Arild Andreassen <arildsa@hotmail.com>
2023-02-03 20:16:39 -0700

    updated api endpoint (#27)

--- src/env/dev.config.ts
+++ src/env/dev.config.ts
@@ -1,4 +1,4 @@
 export const config = {
     ENV: "local",
-    BASE_URL: 'https://llwl85el23.execute-api.us-west-1.amazonaws.com/dev/'
+    BASE_URL: 'https://tjwuadx56m.execute-api.us-west-1.amazonaws.com/dev'
 }
